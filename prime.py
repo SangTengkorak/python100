c3d77a3d0bdcd540d5b613788a2a8400d6a92096
SangTengkorak <62349215+SangTengkorak@users.noreply.github.com>
2021-04-06 20:40:52 +0700

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,7 +1,12 @@
+# SangTengkorak
+# April 06 2021
+
 def is_prime(num):
+    # Check from 2 to input number if there is any value that can divided with number aside of 1 and inputed number itself
     for i in range(2, num):
         if num % i == 0:
             print(f'{num} not a prime')
+            # stop division once condition not meet
             break
         else:
             print(f'{num} a prime')

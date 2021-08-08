f4d1563ba3a61ea2ad55d7e15bd97dcff5bea047
muhammad.pratomo <muhammad.pratomo@gojek.com>
2021-08-08 07:46:08 +0700

    Adds pandas lab

---
+++ pandas_lab/pandas_1try.py
@@ -0,0 +1,14 @@
+# SangTengkorak
+
+# importing pandas library
+import pandas as pd
+
+# reading the csv file
+df = pd.read_csv("data.csv")
+
+# # reading the xlsx file
+# df1 = pd.read_excel("data.xlsx")
+
+print(df.head())
+
+# print(df1.head())
